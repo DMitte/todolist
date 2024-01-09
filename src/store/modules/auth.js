50723c5171fe7df5127533b20368e1ef69e68f03
Dany Mitte <57202696+DMitte@users.noreply.github.com>
2024-01-09 17:56:42 -0500

    Update auth.js

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -15,7 +15,7 @@ export default {
     async register(usuario) {
       try {
         const res = await fetch(
-          "https://todolistback.dmitte.repl.co/api/user/register",
+          "https://todolist-back-65ad.onrender.com/api/user/register",
           {
             method: "POST",
             headers: { "Content-Type": "application/json" },
@@ -31,7 +31,7 @@ export default {
     async login({ commit }, usuario) {
       try {
         const res = await fetch(
-          "https://todolistback.dmitte.repl.co/api/user/login",
+          "https://todolist-back-65ad.onrender.com/api/user/login",
           {
             method: "POST",
             headers: { "Content-Type": "application/json" },
